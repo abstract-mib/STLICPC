4b954bc24d32a0eb68b3a6fef34177c521e26a12
abstract-mib <abstract-mib@users.noreply.github.com>
2017-10-04 18:01:30 +0530

    Adding template class to add two numbers

---
+++ add_class_template.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+template <class T>
+class something {
+	public:
+		T a[2];
+		T add();
+		something();
+};
+template <class T>
+something<T>::something() {
+	cin >> a[0] >> a[1];
+}
+template <class T>
+T something<T> :: add() {
+	return a[0] + a[1];
+}
+
+int main(void) {
+	something<float> floatsomething;
+	cout << floatsomething.add();
+	return 0;
+}
