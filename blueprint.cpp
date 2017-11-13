1fd788b34eb82ceb020655511a2485af52851deb
abstract-mib <abstract-mib@users.noreply.github.com>
2017-11-13 23:57:49 +0530

    Adding sorts algorithms in a header file and all.

--- blueprint.cpp
+++ blueprint.cpp
@@ -1,6 +1,26 @@
+#include <algorithm>
+#include <deque>
 #include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <string>
+#include <vector>
+
+#define FOR(I, A, B) for(I = (A); I < (B); ++I)
+#define REP(I, N) FOR(I, 0, N)
+#define ALL(A) (A).begin(), (A).end()
+
+typedef long long int ll;
+typedef unsigned long long int ull;
 
 using namespace std;
+
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+typedef vector<bool> vb;
 
 int main(void) {
 
---
+++ sorts/sorts.h
@@ -0,0 +1,18 @@
+#ifndef SORT
+#define SORT
+
+/**
+  * All types of sorting function resides here
+  */
+
+class sort {
+	
+	// .merge
+	// .bubble
+	// .insertion
+	// .quick
+	// .selection
+}
+
+#endif
+
